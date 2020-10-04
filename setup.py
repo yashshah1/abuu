f97d319fbac94ba86758f32342d8bebadf2dc9ed
B.S.N. Abhiram <68442741+abhirambsn@users.noreply.github.com>
2020-10-04 09:13:04 +0530

    Update setup.py
    Co-authored-by: Yash Shah <yashah1234@gmail.com>

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="abuu",
-    version="1.0.0",
+    version=abuu.__version__,
     description="ABUU - Another Bunch of Uselessfull Utils",
     long_description=README,
     long_description_content_type="text/markdown",
